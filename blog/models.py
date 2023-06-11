fadb0fb2e566f541172257e5be155402a3f08370
Myra <myratambwe@gmail.com>
2023-06-11 07:13:22 +0200

    Add Sphinx documentation and modify Django files

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-# Default behaviour - Django creates primary keys for you
+    """
+    Model representing a blog post.
+
+    Fields:
+    - title (CharField): The title of the blog post.
+    - body (TextField): The content of the blog post.
+    - signature (CharField): The signature or quote associated with the blog post.
+    - date (DateTimeField): The date and time of the blog post creation.
+
+    Methods:
+    - __str__: This returns the string representation of the blog post (the title).
+    """
     title = models.CharField(max_length=140)
     body = models.TextField()
     signature = models.CharField(max_length=140, default="The future belongs to those who believe in the beauty of their dreams.")
@@ -10,5 +21,5 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
+
 
--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,19 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required(login_url='/login/')
 def blog_home(request):
+    """
+    The view function for the blog homepage.
+
+    This view displays a list of all blog posts.
+    Only authenticated users can access this page.
+
+    Parameters:
+    - request: HttpRequest object representing the current request.
+
+    Returns:
+    - HttpResponse object rendering the 'blog.html' template,
+      including the list of blog posts as context.
+
+    """
     posts = Post.objects.all()
     return render(request, 'blog.html', {'posts': posts})
-
